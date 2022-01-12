ac22220b7aa21993fd797e25d65f6deaa312edff
argun8394 <argundoganali@gmail.com>
2022-01-13 02:20:17 +0300

    useState

---
+++ src/FunctionalComponent.js
@@ -0,0 +1,19 @@
+import React,{useState} from "react";
+
+const FunctionalComponent=()=>{
+    const [counter,setCounter]=useState(0);
+
+    const increase =()=>{
+        setCounter(counter+1);
+    }
+
+    return (
+    <div className="function">
+        <h2>Functional Component</h2>
+        <p>Counter : {counter}</p>
+        <button onClick={()=>increase()}>Increase</button>
+    </div>
+    )
+};
+
+export default FunctionalComponent;
