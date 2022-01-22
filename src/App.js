8c541e62505bf892966b9ad6a921f8a5a49f3eaa
argun8394 <argundoganali@gmail.com>
2022-01-22 14:42:04 +0300

    useRef_inputRef

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import FunctionalComponent from './FunctionalComponent';
-import {useState} from "react";
+// import {useState} from "react";
 
 
 function App() {
-const [isVisible,setVisible] =useState(true);
+// const [isVisible,setVisible] =useState(true);
 
   return (
     <div className="App">
-      <button onClick={() =>setVisible(!isVisible)}>Visibility</button>
+      {/* <button onClick={() =>setVisible(!isVisible)}>Visibility</button> */}
     
-    {isVisible ? <FunctionalComponent/> : null}
+    {true ? <FunctionalComponent/> : null}
     </div>
   );
 }
--- src/FunctionalComponent.js
+++ src/FunctionalComponent.js
@@ -1,24 +1,38 @@
-import React, {useState, useEffect} from "react";
+import React, {useState,useRef} from "react";
 
 const FunctionalComponent=()=>{
+    const inputRef =useRef(null);
     const [counter,setCounter]=useState(0);
+    const [text,setText]=useState("");
 
-    useEffect(()=>{
-        console.log("useEffect");
-        return()=>{console.log("func comp WillUnmount")}
-    },[counter]);
+    // useEffect(()=>{
+    //     console.log("useEffect");
+    //     return()=>{console.log("func comp WillUnmount")}
+    // },[counter]);
 
     const increase =()=>{
         setCounter(counter+1);
     }
 
+const changeBGColor =()=>{
+    // console.log(inputRef.current.value);// useRef ile değerimize ulaştık
+    inputRef.current.parentElement.style.backgroundColor=text;//inputRef ile parent element background color rengini değiştirebildik
+}
 
+const handleText=(e)=>{
+setText(e.target.value)
+};
 
     return (
     <div className="function">
         <h2>Functional Component</h2>
         <p>Counter : {counter}</p>
         <button onClick={increase}>Increase</button>
+
+        <hr/>
+        <input ref={inputRef} type="text" value={text} onChange={handleText} />
+        <button onClick={()=>changeBGColor()}>BG Color</button>
+
     </div>
     )
 };
